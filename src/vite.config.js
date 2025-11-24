fdff677a0b1f9d8f6dc3d4bfbae468aa0de1c66f
BrutusAi <pascalhares@gmail.com>
2025-11-24 23:32:37 +0100

    ci: deploy to github pages

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({mode}) => ({
+  base: process.env.BASE_PATH || '/',
   plugins: [react()],
   define: {
     __firebase_config: JSON.stringify(process.env.VITE_FIREBASE_CONFIG || '{}'),
